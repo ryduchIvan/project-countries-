b6987b156cda81f558e8e766a2353bda3514b236
Ivan Rudnichenki <rudnicenko.ivan@gmail.com>
2022-10-09 21:27:22 +0300

    add redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,21 @@ import React from 'react';
 import * as ReactDOMClient from "react-dom/client";
 import {Provider} from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
 import './index.css';
 import App from './App';
-
-import {store} from './store';
-
+import {store, persistor} from './store';
+import { PersistGate } from 'redux-persist/integration/react'
 
 const rootElement = document.getElementById("root") as HTMLDivElement;
 const root = ReactDOMClient.createRoot(rootElement);
-
 root.render(
   <React.StrictMode>
+    <PersistGate persistor={persistor}>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
+    </PersistGate>
   </React.StrictMode>
 );
--- src/store.ts
+++ src/store.ts
@@ -1,20 +1,29 @@
 import axios from 'axios';
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
-
 import * as api from 'config';
 import { themeReducer } from 'features/theme/theme-slice';
 import { controlsReducer } from 'features/controls/controls-slice';
 import { countryReducer } from 'features/countries/countries-slice';
 import { detailsReducer } from 'features/details/details-slice';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 
-export const store = configureStore({
-  reducer: {
+const rootReducer = combineReducers(
+  {
     theme: themeReducer,
     controls: controlsReducer,
     countries: countryReducer,
     details: detailsReducer,
-  },
+  }
+)
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+export const store = configureStore({
+  reducer: persistedReducer,
   devTools: true,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     thunk: {
@@ -30,3 +39,4 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch: () => AppDispatch = useDispatch;
+export const persistor = persistStore(store);
